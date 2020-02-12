a23562e081621d1dc6a7538c45443f435f0f0a59
Artur <33733651+keeqler@users.noreply.github.com>
2020-02-12 15:36:17 -0300

    Add shebang
    I forgot it :/

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget -O code.tar.gz https://update.code.visualstudio.com/latest/linux-x64/stable
 tar -xvzf code.tar.gz
 sudo rm -Rf /opt/code/* /bin/code
@@ -6,4 +8,3 @@ rm -Rf code.tar.gz VSCode-linux-x64/
 sudo ln -s /opt/code/bin/code /bin/code
 echo "Visual Studio Code has been updated!"
 echo "Type in "code" to open it up"
-
