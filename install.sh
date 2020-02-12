de239451c2b6a4003cec244a9af975a89ec9a865
Artur <33733651+keeqler@users.noreply.github.com>
2020-02-12 15:34:25 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+wget -O code.tar.gz https://update.code.visualstudio.com/latest/linux-x64/stable
+tar -xvzf code.tar.gz
+sudo rm -Rf /opt/code/* /bin/code
+sudo mv VSCode-linux-x64/* /opt/code/
+rm -Rf code.tar.gz VSCode-linux-x64/
+sudo ln -s /opt/code/bin/code /bin/code
+echo "Visual Studio Code has been updated!"
+echo "Type in "code" to open it up"
+
