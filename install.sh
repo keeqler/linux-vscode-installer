9c58b501e24f7327e902fb99faa801f717f75e91
Artur <33733651+keeqler@users.noreply.github.com>
2020-02-14 19:05:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ sudo rm -Rf /opt/code/* /bin/code
 sudo mv VSCode-linux-x64/* /opt/code/
 rm -Rf code.tar.gz VSCode-linux-x64/
 sudo ln -s /opt/code/bin/code /bin/code
-echo "Visual Studio Code has been updated!"
-echo "Type in "code" to open it up"
+echo 'Visual Studio Code has been updated!'
+echo 'Type in "code" to open it up'
