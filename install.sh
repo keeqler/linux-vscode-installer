1bd09efa1351cdb29aa06f5e16b63ed8838796a5
Keepler <arturnunespe@gmail.com>
2020-07-10 13:58:49 -0300

    Recreate /opt/code after deleting it

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 wget -O code.tar.gz https://update.code.visualstudio.com/latest/linux-x64/stable
 tar -xvzf code.tar.gz
-sudo rm -Rf /opt/code/* /bin/code
+sudo rm -Rf /opt/code /bin/code
+sudo mkdir /opt/code
 sudo mv VSCode-linux-x64/* /opt/code/
 rm -Rf code.tar.gz VSCode-linux-x64/
 sudo ln -s /opt/code/bin/code /bin/code
